7185bbf86e1c4aeccb2c7049ac4dde8fe644c17a
Muhammad Amirul Zulkapri <amirulkitul@gmail.com>
2021-10-29 17:01:06 +0800

    add heroku config for django

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn etauliahph.wsgi
--- etauliahph/settings.py
+++ etauliahph/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,5 +127,5 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+django_heroku.settings(locals())
 
