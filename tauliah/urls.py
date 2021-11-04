aef1a42f6e17d87fcd7e987f6b719d6ff95e4977
Muhammad Amirul Zulkapri <amirulkitul@gmail.com>
2021-11-04 21:44:13 +0800

    new

--- tauliah/urls.py
+++ tauliah/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
 
     #MAIN REDIRECT URL
     path('',views.main_page,name='main_page'),
-    path('tauliah/<str:cawangan_list>',views.download_sijil_tauliah, name="sijil_tauliah"),
-    path('tauliah/<str:cawangan_list>',views.download_sijil_kosong, name="sijil_kosong"),
+    path('cawangan/<str:cawangan_list>',views.senarai_kelas,name='senarai_kelas'),
+    path('tauliah/<str:cawangan_list>/esijil',views.download_sijil_tauliah, name="sijil_tauliah"),
+    path('tauliah/<str:cawangan_list>/esijil/<str:kelas_list>/kelas',views.download_sijil_tauliah_bykelas, name="download_sijil_tauliah_bykelas"),
+    path('tauliah/<str:cawangan_list>/sijilkosong',views.download_sijil_kosong, name="sijil_kosong"),
+    path('tauliah/<int:pk>/individu',views.download_sijil_individu, name="download_sijil_individu"),
 
 ]
--- tauliah/views.py
+++ tauliah/views.py
@@ -13,10 +13,27 @@ def main_page(request):
     context={}
     tp_list = TP.objects.all().order_by('cawangan','kelas','status')
     cawangan_list = TP.objects.all().values('cawangan').distinct()
-    print(cawangan_list)
     context['cawangan_list'] = cawangan_list
     context['tp_list'] = tp_list
     return render(request , 'main_page.html' ,{'context':context})
+
+@login_required(login_url='/admin/login/?next=/admin/')
+def senarai_kelas(request,cawangan_list):
+    context = {}
+    kelas_list = TP.objects.filter(cawangan=cawangan_list).values('kelas','cawangan').distinct()
+    tp_list = TP.objects.filter(cawangan=cawangan_list).order_by('cawangan','kelas','status')
+    context['kelas_list'] = kelas_list
+    context['tp_list'] = tp_list
+    return render(request , 'senarai_kelas.html' ,{'context':context})
+
+@login_required(login_url='/admin/login/?next=/admin/')
+def download_sijil_tauliah_bykelas(request,cawangan_list,kelas_list):
+    context = {}
+    tp_list = TP.objects.filter(kelas=kelas_list,cawangan=cawangan_list).order_by('cawangan','kelas','status')
+    context['list'] = tp_list
+    context['BASE_DIR'] = BASE_DIR
+    return Render.render(kelas_list+'.pdf','sijil/sijil.html', context)
+
 
 @login_required(login_url='/admin/login/?next=/admin/')
 def download_sijil_tauliah(request,cawangan_list):
@@ -33,3 +50,11 @@ def download_sijil_kosong(request,cawangan_list):
     context['list'] = tp_list
     context['BASE_DIR'] = BASE_DIR
     return Render.render(cawangan_list+'.pdf','sijil/sijil_kosong.html', context)
+
+@login_required(login_url='/admin/login/?next=/admin/')
+def download_sijil_individu(request,pk):
+    context = {}
+    tp_list = TP.objects.filter(pk=pk)
+    context['list'] = tp_list
+    context['BASE_DIR'] = BASE_DIR
+    return Render.render(tp_list[0].nama+'.pdf','sijil/sijil.html', context)
